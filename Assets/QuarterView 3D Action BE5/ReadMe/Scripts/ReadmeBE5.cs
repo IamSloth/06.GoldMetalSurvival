23a4d36c0545b3b8460e60b5c590a51fdf6595aa
IamSloth <deathbed0104@gmail.com>
2022-11-11 11:33:39 +0900

    Initial commit

---
+++ Assets/QuarterView 3D Action BE5/ReadMe/Scripts/ReadmeBE5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace QuarterView_3D_Action_BE5.ReadMe.Scripts
+{
+	public class ReadmeBE5 : ScriptableObject {
+		public Texture2D icon;
+		public string title;
+		public Section[] sections;
+		public bool loadedLayout;
+	
+		[Serializable]
+		public class Section {
+			public string heading, text, linkText, url;
+		}
+	}
+}
